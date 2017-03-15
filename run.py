53a43a7eb29042cbd595c4359fe147c8004ce449
Alexey Mironov <s18b3_mironov@179.ru>
2017-03-15 22:44:36 +0300

    add submit check

--- run.py
+++ run.py
@@ -1,62 +1,179 @@
-# coding=utf-8
 import requests
+from lxml import html
 import time
 
 
 def post_request(url, data=None, cookies=None, files=None, timeout=10):
+    """
+    post request with exception handling
+    :param url: url to make request
+    :param data: data to send
+    :param cookies: cookies to send
+    :param files: files to send
+    :param timeout: close connect after timeout seconds
+    :return: request object if request make without exceptions. Else None
+    """
     try:
         return requests.post(url, data=data, cookies=cookies, files=files, timeout=timeout)
     except requests.exceptions.ConnectTimeout:
-        print('ConnectTimeout')
+        pass
     except requests.exceptions.ReadTimeout:
-        print('ReadTimeout')
+        pass
 
 
 def get_request(url, data=None, cookies=None, timeout=10):
+    """
+    get request with exception handling
+    :param url: url to make request
+    :param data: data to send
+    :param cookies: cookies to send
+    :param timeout: close connect after timeout seconds
+    :return: request object if request make without exceptions. Else None
+    """
     try:
         return requests.get(url, data=data, cookies=cookies, timeout=timeout)
     except requests.exceptions.ConnectTimeout:
-        print('ConnectTimeout')
+        pass
     except requests.exceptions.ReadTimeout:
-        print('ReadTimeout')
-
-
-def auth(username, password):
-    url = 'http://informatics.msk.ru/login/index.php'
-    r = post_request(url, dict(username=username, password=password), cookies={})
-    while r is None or len(r.history) == 0:
-        time.sleep(1)
-        r = post_request(url, dict(username=username, password=password), cookies={})
-    print('Auth success')
-    return r.history[0].cookies
+        pass
 
 
 def get_page(url, data=None, cookies=None):
+    """
+    Load page until it loaded success
+    :param url: page url
+    :param data: data to request
+    :param cookies: cookies to request
+    :return: request object
+    """
     r = get_request(url, data=data, cookies=cookies)
     while r is None:
         time.sleep(1)
         r = get_request(url, data=data, cookies=cookies)
     return r
 
-
-def get_test_id(problem_id, cookies):
-    try:
-        page = get_page('http://informatics.msk.ru/mod/statements/view3.php?id={}'.format(problem_id), cookies=cookies).text
-        j = page.index('/py/problem/') + len('/py/problem/')
-        k = j
-        while page[k].isdigit():
-            k += 1
-        return int(page[j:k])
-    except:
+def auth(username, password):
+    """
+    Authenticates at informatics.msk.ru
+    :param username: username at informatics
+    :param password: password at informatics
+    :return: (cookies with session key, user id at informatics)
+    """
+    url = 'http://informatics.msk.ru/login/index.php'
+    r = post_request(url, dict(username=username, password=password), cookies={})
+    while r is None:
         time.sleep(1)
-        return get_test_id(problem_id, cookies)
+        r = post_request(url, dict(username=username, password=password), cookies={})
+    page = html.fromstring(r.text)
+    id_data = page.xpath('//*[@id="footer"]/div/a')[0]
+    user_id = id_data.attrib['href']\
+        .replace('http://informatics.msk.ru/user/view.php?id=', '').replace('&course=1', '')
+    return r.history[0].cookies, user_id
+
+class Run:
+    """
+    make run to submit at informatics.msk.ru
+    """
+    def __init__(self, problem_id, file_patch, lang_id, cookies, user_id):
+        """
+        :param problem_id: problem number
+        :param file_patch: absolute or relative path
+        :param lang_id: lang_id at informatics
+        :param cookies: session auth cookies
+        :param user_id: user id at informatics
+        """
+        self.problem_id = problem_id
+        self.file = file_patch
+        self.lang_id = lang_id
+        self.cookies = cookies
+        self.user_id = user_id
+        self.submits = set()
+
+    def get_submits(self):
+        """
+        get submits from informatics with curent problem, user id and lang
+        :return: table from informatics
+        """
+        url = 'http://informatics.msk.ru/moodle/ajax/ajax.php?problem_id={}&from_timestamp=-1&to_timestamp=-1&group_id=0&user_id={}&lang_id={}&status_id=-1&statement_id=0&objectName=submits&count=500&with_comment=&page=0&action=getHTMLTable' \
+            .format(self.problem_id, self.user_id, self.lang_id)
+        r = get_page(url)
+        text = r.json()['result']['text']
+        page = html.fromstring(text)
+        rows = page.xpath('//table')[0].findall('tr')
+        data = list()
+        for row in rows:
+            data.append([c.text for c in row.getchildren()])
+        return data
 
 
-def submit(file, problem_id, cookies):
-    url = 'http://informatics.msk.ru/py/problem/{}/submit'.format(problem_id)
-    while True:
-        time.sleep(1)
-        r = post_request(url, data={'lang_id': 3 if (file.split('.')[-1] == 'cpp') else 27}, cookies=cookies, files=dict(file=open(file)), timeout=1)
+    def get_source(self, run):
+        """
+        get source code
+        :param run: run id which source need
+        :return: string with source code
+        """
+        contest_id, run_id = run.split('-')
+        url = 'http://informatics.msk.ru/moodle/ajax/ajax_file.php?objectName=source&contest_id={}&run_id={}' \
+            .format(contest_id, run_id)
+        r = get_page(url, cookies=self.cookies)
+        page = html.fromstring(r.text)
+        data = page.xpath('//div/textarea')[0]
+        return data.text
 
 
-submit('Source.cpp', 111134, auth('login', 'password'))
+    def has_submitted(self):
+        """
+        Check has this source already submitted
+        :return: run id if has. Else None
+        """
+        source = open(self.file).read()
+        submits = self.get_submits()
+        for submit in submits[1:]:
+            submit_id = submit[0]
+            if submit_id not in self.submits:
+                if self.get_source(submit_id) == source:
+                    return submit_id
+                else:
+                   self.submits.add(submit_id)
+
+
+    def submit(self):
+        """
+        submit run to informatics
+        :return: run id
+        """
+        url = 'http://informatics.msk.ru/py/problem/{}/submit'.format(self.problem_id)
+        was_submitted = self.has_submitted()
+        while was_submitted is None:
+            time.sleep(1)
+            r = post_request(url, data={'lang_id': self.lang_id}, cookies=self.cookies,
+                             files=dict(file=open(self.file)), timeout=1)
+            was_submitted = self.has_submitted()
+        return was_submitted
+
+
+if __name__ == '__main__':
+    login = input('login: ')
+    password = input('pasword: ')
+    cookies, user_id = auth(login, password)
+    print('auth success')
+    problem_id = input('problem number(from title): ')
+    file = input('file patch: ')
+    lang_id = input("""Language                 id
+    Free Pascal 2.6.2:        1
+    GNU C 4.9                 2
+    GNU C++ 4.9               3
+    Borland Delphi 6 - 14.5   8
+    Java JDK 1.7             18
+    PHP 5.2.17               22
+    Python 2.7               23
+    Perl 5.10.1              24
+    Mono C# 2.10.8.0         25
+    Ruby 1.8.7               26
+    Python 3.3               27
+    Haskell GHC 7.4.2        28
+    FreeBASIC 1.00.0         29
+    PascalABC 1.8.0.496      30
+    lang id: """)
+    submit = Run(problem_id, file, lang_id, cookies, user_id).submit()
+    print("Submitted, run_id: " + submit)
